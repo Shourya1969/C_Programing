60dd8425f8b61ad32e85d1b1e85d6ed2aee9402b
Shourya1969 <shourya2552@gmail.com>
2026-09-25 23:23:34 +0530

    Create Experiment5.c

---
+++ Experiment5.c
@@ -0,0 +1,14 @@
+include <stdio.h>
+
+int main() {
+    // Single line comment
+
+    /* This is a 
+       multi-line comment */
+
+    int value = 25;
+
+    printf("Value = %d\n", value);
+
+    return 0;
+}
